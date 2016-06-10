89f5845a2b33ae982f417fbba01539e565f1cd9c
Your Name <you@example.com>
2016-06-10 15:32:08 +0200

    code embellishment.

--- src/main/java/net/starschema/tabadmin_cli/Main.java
+++ src/main/java/net/starschema/tabadmin_cli/Main.java
@@ -3,22 +3,17 @@ package net.starschema.tabadmin_cli;
 
 import org.apache.log4j.Logger;
 
-import java.io.IOException;
-
 public class Main {
 
     final static Logger logger = Logger.getLogger(Main.class);
-
 
     public static void main(String[] args) {
 
         try {
             CliControl.restartWorkers();
-        } catch (IOException e) {
-            System.out.println("Cannot connect to the balancer-manager.");
-            logger.fatal("Cannot connect to the balancer-manager.");
         } catch (Exception e) {
-            System.out.println("Fatal: "+e.toString());
+            System.out.println("Fatal: "+e.getMessage());
+            logger.fatal(e.toString(), e);
         }
     }
 }
